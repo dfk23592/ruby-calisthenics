6041dfcbd4a4fcf9881c1e8f0fa34f79fff3f1f1
tbriggs <tbriggs@debian.local>
2017-09-09 11:51:30 -0400

    adding GemFile

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,9 +1,11 @@
 class Dessert
+  attr_accessor :name, :calories
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
   end
   def healthy?
-    # your code here
+    @calories < 200
   end
   def delicious?
     # your code here
